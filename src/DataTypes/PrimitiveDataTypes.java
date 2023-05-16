83fb51a0c9349384d3626a95f8f0cdeef677ad84
Deekshitha Alisetty <89872822+SE0012@users.noreply.github.com>
2023-05-16 01:28:10 -0500

    Initial commit

---
+++ src/DataTypes/PrimitiveDataTypes.java
@@ -0,0 +1,28 @@
+package DataTypes;
+
+public class AllDataTypes {
+    public static void main(String[] args){
+        //Primitive DataTypes
+
+        //integer types - byte, short,int, long
+        byte  a= 100; // 1-byte
+        short b = 2000; // 2-bytes
+        int c = 120000; // 4- bytes
+        long  e = 6789065425689; // 8 -Bytes
+
+        // Floating Points
+        float d= 23.00f; //stores 6 decimal digits
+        double f = 5678902.12d; //stores 1
+
+        //characters
+        char  g = 'A';
+        // Boolean Types
+        boolean h = true;
+
+        String i =  "Siri";
+
+
+    }
+
+
+}
