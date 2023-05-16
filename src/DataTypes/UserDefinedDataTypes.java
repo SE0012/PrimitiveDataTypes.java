6d63bce5626c9ac96d64fba290d7f64c3bcd7877
Deekshitha Alisetty <89872822+SE0012@users.noreply.github.com>
2023-05-16 01:49:39 -0500

    Updated
    Signed-off-by: Deekshitha Alisetty <89872822+SE0012@users.noreply.github.com>

---
+++ src/DataTypes/UserDefinedDataTypes.java
@@ -0,0 +1,15 @@
+package DataTypes;
+
+public class UserDefinedDataTypes {
+    public static void main(String[] args) {
+        //String,class,object,interface,Array
+        //String - it won't have any length
+        String myName = "Deekshitha";
+        String myJobRole = "Java Developer";
+        String companyName = "Delloite";
+        System.out.println(myName);
+        System.out.println(myJobRole);
+        System.out.println(companyName);
+
+    }
+}
